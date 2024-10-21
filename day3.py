e85830e89b02bf37293ba021535d561da342dbe2
Siman <tarapatisiman1@gmail.com>
2024-10-21 18:54:23 +0400

    made some changes in the logic

--- day3.py
+++ day3.py
@@ -1,16 +1,18 @@
-n=float(input("Enter your marks:"))
+n=float(input("Enter your marks out of 100:"))
 
-if 91<n<=100:
+if 90<n<=100:
     print("You got AA grade..")
-elif 81<n<=90:
+elif 80<n<=90:
     print("You got AB grade..")
-elif 71<n<=80:
+elif 70<n<=80:
     print("You got BB grade..")
-elif 61<n<=70:
+elif 60<n<=70:
     print("You got BC grade..")
-elif 51<n<=60:
+elif 50<n<=60:
     print("You got CD grade..")
-elif 41<n<=50:
+elif 40<n<=50:
     print("You got DD grade..")
 elif n<=40:
     print("You failed..")
+else:
+    print("You have entered your marks greater than 100.Enter marks between 1 to 100")
