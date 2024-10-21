2f77f3776d9efba7090ee575389c7edab70f9230
Siman <tarapatisiman1@gmail.com>
2024-10-21 12:37:23 +0400

    check your grade

---
+++ day3.py
@@ -0,0 +1,16 @@
+n=float(input("Enter your marks:"))
+
+if 91<n<=100:
+    print("You got AA grade..")
+elif 81<n<=90:
+    print("You got AB grade..")
+elif 71<n<=80:
+    print("You got BB grade..")
+elif 61<n<=70:
+    print("You got BC grade..")
+elif 51<n<=60:
+    print("You got CD grade..")
+elif 41<n<=50:
+    print("You got DD grade..")
+elif n<=40:
+    print("You failed..")
