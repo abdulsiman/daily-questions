d52e5043f682bd8d04e631b2977b29e281184c23
Siman <tarapatisiman1@gmail.com>
2024-11-07 22:01:30 +0400

    checks prime no b/w the range

--- day11.py
+++ day11.py
@@ -1,10 +1,10 @@
 start=int(input("Enter the starting number:"))
 end=int(input("Enter the ending number:"))
-for num in range(start,end+1):
-    if num>1:
-        for i in range(2,num):
-            if num%i==0:
+for number in range(start,end+1):
+    if number>1:
+        for i in range(2,number):
+            if number%i==0:
                 break
         else:
-            print(num)
+            print(number)
         
