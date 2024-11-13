47fa3650e1d5c6f3aa8e730b4643debe7094492e
Siman <tarapatisiman1@gmail.com>
2024-11-13 14:33:49 +0400

    finding max and min numbers

---
+++ day15.py
@@ -0,0 +1,19 @@
+num1=int(input("Enter your num1:"))
+num2=int(input("Enter your num2:"))
+num3=int(input("Enter your num3:"))
+def maximum(num1,num2,num3):
+    if(num1>=num2 and num1>=num3):
+        print(f"{num1} is maximum")
+    elif(num2>=num1 and num2>=num3):
+        print(f"{num2} is maximum")
+    elif(num3>=num2 and num3>=num1):
+        print(f"{num3} is maximum")
+def manimum(num1,num2,num3):
+    if(num1<=num2 and num1<=num3):
+        print(f"{num1} is manimum")
+    elif(num2<=num1 and num2<=num3):
+        print(f"{num2} is manimum")
+    elif(num3<=num2 and num3<=num1):
+        print(f"{num3} is manimum")
+maximum(num1,num2,num3)
+manimum(num1,num2,num3)
